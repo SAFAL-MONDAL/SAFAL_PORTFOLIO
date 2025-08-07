4f1db265551e83cfb2c9b869c206c3ed96590293
SAFAL MONDAL <157379029+SAFAL-MONDAL@users.noreply.github.com>
2025-08-07 18:17:04 +0530

    vercel.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://',
+        target: 'http:// || import.meta.env.VITE_API_BASE_URL ',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
